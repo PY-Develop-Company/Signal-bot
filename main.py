f21a94f1cf766f5563ea790853e7d2b451ea7688
Kovalchuk <sashakovalchuk1955@gmail.com>
2023-09-20 22:22:28 +0300

    Fi

--- main.py
+++ main.py
@@ -19,19 +19,19 @@ dp = Dispatcher(bot)
 
 db_path = "users/db.txt"
 
-manager_username = "@test_nano"
-managers_id = [5359645780]
+manager_username = "@justsashakovalchuk"
+managers_id = [5308615063]
 manager_url = f"https://t.me/{manager_username[1:]}"
 
-start_search_manager = "VIP ?"
+start_search_manager = "Підтвердження VIP-акаунтів"
 check_text = "Який видати статус користувачу"
 
 for_vip_text = "Для отримання VIP..."
 you_have_vip_text = "У вас вже активний vip"
 
-vip_status = "vip"
-nf_vip_status = "nf"
-none_status = "none"
+vip_status = "Надати VIP статус"
+nf_vip_status = "Не виконано умов на VIP статус"
+none_status = "Надати NONE статус"
 wait_status = 'Очікуємо перевірки'
 
 
@@ -117,13 +117,13 @@ async def search_no_vip(message):
     return "Всі заявки у vip оброблені"
 
 
-async def edit_user_menu(message):
+async def accept_vip_accounts_menu(message):
     await message.answer(await search_no_vip(message))
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton(vip_status)
     item2 = types.KeyboardButton(nf_vip_status)
     item3 = types.KeyboardButton(none_status)
-    markup.add(item1,item2,item3)
+    markup.add(item1, item2, item3)
     await message.answer("Виберіть статус :", reply_markup=markup)
 
 
@@ -136,7 +136,7 @@ async def manager_menu(message):
 
 async def vip_main_menu(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("связаться с менеджером")
+    item1 = types.KeyboardButton("зв'язатись з менеджером")
     markup.add(item1)
     await message.answer("Оберіть опцію:", reply_markup=markup)
     pass
@@ -144,9 +144,9 @@ async def vip_main_menu(message):
 
 async def not_vip_main_menu(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("хочу в вип")
-    item2 = types.KeyboardButton("проверить")
-    item3 = types.KeyboardButton("связаться с менеджером")
+    item1 = types.KeyboardButton("хочу в віп")
+    item2 = types.KeyboardButton("перевірити")
+    item3 = types.KeyboardButton("зв'язатись з менеджером")
     markup.add(item1, item2, item3)
     await message.answer("Оберіть опцію:", reply_markup=markup)
 
@@ -155,7 +155,7 @@ async def add_user(message):
     data = read_file(db_path)
     user_exists = any(user['id'] == message.from_user.id for user in data)
     if user_exists:
-        ...
+        message.answer("Користувач вже доданий!")
     else:
         full_name = f"{message.from_user.first_name} {message.from_user.last_name}"
         bufer_user = {"id": message.from_user.id, "name": full_name, "status": "none"}
@@ -184,36 +184,38 @@ async def create_user(message):
 
 @dp.message_handler(content_types=["text"])
 async def handle_media(message: types.Message):
-    if message.text == start_search_manager and message.from_user.id in managers_id:
-        await edit_user_menu(message)
+    if message.from_user.id in managers_id:
+        if message.text == start_search_manager:
+            await accept_vip_accounts_menu(message)
 
-    elif message.text == vip_status and message.from_user.id in managers_id:
-        test = await check_manager_status(message)
-        print(" >>  ",test)
-        await update_status_user(test,vip_status)
-    elif message.text == nf_vip_status and message.from_user.id in managers_id:
-        ...
-    elif message.text == none_status and message.from_user.id in managers_id:
-        ...
-
-    elif message.text == "хочу в вип":
+        elif message.text == vip_status:
+            test = await check_manager_status(message)
+            print(" >>  ",test)
+            await update_status_user(test, vip_status)
+        elif message.text == nf_vip_status:
+            test = await check_manager_status(message)
+            await update_status_user(test, nf_vip_status)
+        elif message.text == none_status:
+            test = await check_manager_status(message)
+            await update_status_user(test, none_status)
+    elif message.text == "хочу в віп":
         if has_user_status(message.from_user.id, vip_status):
             await message.answer(you_have_vip_text)
             await vip_main_menu(message)
         else:
             await message.answer(for_vip_text)
-    elif message.text == "проверить":
-        if has_user_status(message.from_user.id,vip_status):
+    elif message.text == "перевірити":
+        if has_user_status(message.from_user.id, vip_status):
             await message.answer(you_have_vip_text)
             await vip_main_menu(message)
         else:
             await message.answer(wait_status)
-            await update_status_user(message.from_user.id,wait_status)
-    elif message.text == "связаться с менеджером":
+            await update_status_user(message.from_user.id, wait_status)
+    elif message.text == "зв'язатись з менеджером":
         await message.answer("Контакти менеджера :")
         await message.answer(manager_url)
     else:
-        if has_user_status(message.from_user.id,vip_status):
+        if has_user_status(message.from_user.id, vip_status):
             await vip_main_menu(message)
         else:
             await not_vip_main_menu(message)
