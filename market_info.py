0f85e6deef8001c127954ce476fd9ccd14989050
Kovalchuk <sashakovalchuk1955@gmail.com>
2023-12-09 15:43:32 +0200

    is_market_working weekday fix

--- market_info.py
+++ market_info.py
@@ -18,4 +18,5 @@ def is_trial_ended(trial_end_date):
 
 
 def is_market_working():
-    return min_time_zone_hours <= now_time().hour < max_time_zone_hours
+    time = now_time()
+    return min_time_zone_hours <= time.hour < max_time_zone_hours and time.weekday() < 5
