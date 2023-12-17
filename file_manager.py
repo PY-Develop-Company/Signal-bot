3af4ec15d297b9b5fdc12ffa6948f0e317bf8322
Kovalchuk <sashakovalchuk1955@gmail.com>
2023-12-17 22:16:52 +0200

    file open errors fix and

--- file_manager.py
+++ file_manager.py
@@ -1,16 +1,31 @@
 import json
 import os
+import time
 
+file_error = "FILE_READ_ERROR"
+retry_wait_time = 3
 
 def write_file(url, data):
-    with open(url, "w", encoding="utf-8-sig") as file:
-        file.write(json.dumps(data, ensure_ascii=False))
+    while True:
+        try:
+            with open(url, "w", encoding="utf-8-sig") as file:
+                file.write(json.dumps(data, ensure_ascii=False))
+
+            break
+        except PermissionError as e:
+            print(file_error, e)
+            time.sleep(retry_wait_time)
 
 
 def read_file(url):
-    with open(url, 'r', encoding="utf-8-sig") as file:
-        result = json.loads(file.read())
-    return result
+    while True:
+        try:
+            with open(url, 'r', encoding="utf-8-sig") as file:
+                result = json.loads(file.read())
+            return result
+        except PermissionError as e:
+            print(file_error, e)
+            time.sleep(retry_wait_time)
 
 
 def is_file_exists(url):
@@ -18,6 +33,12 @@ def is_file_exists(url):
 
 
 def delete_file_if_exists(url):
-    if is_file_exists(url):
-        os.remove(url)
+    while True:
+        try:
+            if is_file_exists(url):
+                os.remove(url)
 
+            break
+        except PermissionError as e:
+            print(file_error, e)
+            time.sleep(retry_wait_time)
