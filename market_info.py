fd5cd870e22194b3eeba46e309f6c927f60571a8
Kovalchuk <sashakovalchuk1955@gmail.com>
2023-12-22 02:57:53 +0200

    Config for market

--- market_info.py
+++ market_info.py
@@ -1,10 +1,15 @@
 from my_time import date_by_adding_business_days, now_time
 from datetime import datetime
+import configparser
 
-min_time_zone_hours = 10
-max_time_zone_hours = 23
 
-trial_days = 1
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+
+min_time_zone_hours = config["MARKET"].getint("MinTimeZoneHours", 10)
+max_time_zone_hours = config["MARKET"].getint("MaxTimeZoneHours", 23)
+trial_days = config["MARKET"].getint("TrialDays", 1)
 
 
 def get_trial_end_date():
