de2050e6b2b25976fc1e20cf33da067085abaabd
Kovalchuk <sashakovalchuk1955@gmail.com>
2023-12-28 20:28:02 +0200

    fixes

--- manager_module.py
+++ manager_module.py
@@ -1,7 +1,6 @@
 from user_module import *
 from DBModul import *
 
-
 manager_username = "@JOKER_C_VAMI"
 tester_ids = [741867026, 693562775, 5359645780]
 managers_id = [500436965, 741867026, 693562775]
@@ -16,7 +15,7 @@ async def add_manager(id):
     cursor = db_connection.cursor()
     cursor.execute(f"SELECT id FROM {manager_table} WHERE id = ?", (id,))
     existing_user = cursor.fetchone()
-    if not existing_user:
+    if existing_user is None:
         cursor.execute(f'''INSERT INTO {manager_table} (id, status, do, language) VALUES (?, ?, ?, ?)''', (id, none_manager_status, "none", startLanguage))
         db_connection.commit()
 
