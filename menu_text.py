4202ac6d4f862965434bad142906e40fe26ed831
Serhiy <serhiyji@gmail.com>
2025-01-28 14:01:41 +0200

    Revert "fix"
    This reverts commit eb70cb80295f5a6cf266bebd201438fd286eb5ab.

--- menu_text.py
+++ menu_text.py
@@ -25,10 +25,8 @@ binarium_text = "Binarium"
 
 def get_select_id_type_markup():
     select_id_markup = types.ReplyKeyboardMarkup(resize_keyboard=True, keyboard=[
-        [types.KeyboardButton(pocket_option_text)
-         #    , types.KeyboardButton(iq_cent_text),
-         # types.KeyboardButton(binarium_text)
-         ]
+        [types.KeyboardButton(pocket_option_text), types.KeyboardButton(iq_cent_text),
+         types.KeyboardButton(binarium_text)]
     ])
     return select_id_markup
 
